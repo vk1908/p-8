29367eceb80d73ab769ebcb312addfba4c534590
vk1908 <chinnusk3@gmail.com>
2025-06-10 00:02:34 +0530

    first commit

---
+++ p8/src/main/java/com/vk1908/App.java
@@ -0,0 +1,13 @@
+package com.vk1908;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
